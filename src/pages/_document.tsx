6aca326ccc1788fed1e2519772939714705c4a0f
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-02-04 16:57:03 +0000

    removing fonts from document and applying them with next/fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin={"true"}
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
